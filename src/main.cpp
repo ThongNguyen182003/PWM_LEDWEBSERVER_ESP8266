5958f9a44138945a88e5bd3c160fbf47075962db
Thong Nguyen <thong.nguyen1820031956@hvmut.edu.vn>
2023-03-23 21:03:31 +0700

    seccond

--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,72 @@
-
-// Import required libraries
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
+#include <Arduino.h>
+#include<ESP8266WiFi.h>
+#include<ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+#include<ArduinoJson.h>
+#include<Arduino_JSON.h>
 
-// Replace with your network credentials
 const char* ssid = "Redmi";
 const char* password = "hoangyenhi";
 
-const int output = 2;
+// Create async server port 80 ;
 
-String sliderValue = "0";
-
-const char* PARAM_INPUT = "value";
-
-// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+// create websocket
+AsyncWebSocket ws("/ws");
+// set led GPIO
+const int ledPin1 = 14;
+const int ledPin2 = 12;
+const int ledPin3 = 13;
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Thong TEST</title>
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 2.3rem;}
-    p {font-size: 1.9rem;}
-    body {max-width: 400px; margin:0px auto; padding-bottom: 25px;}
-    .slider { -webkit-appearance: none; margin: 14px; width: 360px; height: 25px; background: #FFD65C;
-      outline: none; -webkit-transition: .2s; transition: opacity .2s;}
-    .slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 35px; height: 35px; background: #003249; cursor: pointer;}
-    .slider::-moz-range-thumb { width: 35px; height: 35px; background: #003249; cursor: pointer; } 
-  </style>
-</head>
-<body>
-  <h2>ESP Web Server</h2>
-  <p><span id="textSliderValue">%SLIDERVALUE%</span></p>
-  <p><input type="range" onchange="updateSliderPWM(this)" id="pwmSlider" min="0" max="1023" value="%SLIDERVALUE%" step="1" class="slider"></p>
-<script>
-function updateSliderPWM(element) {
-  var sliderValue = document.getElementById("pwmSlider").value;
-  document.getElementById("textSliderValue").innerHTML = sliderValue;
-  console.log(sliderValue);
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/slider?value="+sliderValue, true);
-  xhr.send();
+String message = "";
+String slidervalue1 = "0";
+String slidervalue2 = "0";
+String slidervalue3 = "0";
+
+int dutyCyle1 ;
+int dutyCyle2 ;
+int dutyCyle3 ;
+JSONVar sliderValues;
+// JSON valiable to hold slider value
+
+//get slider values
+String getSliderValues()
+{
+  sliderValues["slidervalue1"] = String(slidervalue1);
+  sliderValues["slidervalue2"] = String(slidervalue2);
+  sliderValues["slidervalue3"] = String(slidervalue3);
+
+  String jsonString = JSON.stringify(sliderValues);
+  return jsonString;
 }
-</script>
-</body>
-</html>
-)rawliteral";
 
-// Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if (var == "SLIDERVALUE"){
-    return sliderValue;
+void initFS()
+{
+  if(!LittleFS.begin())
+  {
+    Serial.println("Loi khi cai dat Little FS");
   }
-  return String();
+  else
+  {
+    Serial.println("LittleFS thanh cong");
+  }
 }
-
-void setup(){
-  // Serial port for debugging purposes
-  Serial.begin(115200);
-
-  analogWrite(output, sliderValue.toInt());
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+//Khoi tao wifi
+void initWifi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+  Serial.print("Connecting wifi ...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
     delay(1000);
-    Serial.println("Connecting to WiFi..");
   }
-
-  // Print ESP Local IP Address
-  Serial.println(WiFi.localIP());
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-
-  // Send a GET request to <ESP_IP>/slider?value=<inputMessage>
-  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage;
-    // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
-    if (request->hasParam(PARAM_INPUT)) {
-      inputMessage = request->getParam(PARAM_INPUT)->value();
-      sliderValue = inputMessage;
-      analogWrite(output, sliderValue.toInt());
-    }
-    else {
-      inputMessage = "No message sent";
-    }
-    Serial.println(inputMessage);
-    request->send(200, "text/plain", "OK");
-  });
-  
-  // Start server
-  server.begin();
+  Serial.println(WiFi.localIP() );
 }
-  
-void loop() {
-  
+
+void notifyClients(String sliderValues)
+{
+  ws.textAll(sliderValues);
 }
